c0fbb5d3030f26dd8d5f8dd546d167eb7332daa2
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-24 15:15:41 +0530

    ADDED : Reset function for board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,24 @@
 #!/bin/bash -x
 
 echo " ****************************** WELCOME TO TIC TAC TOE GAME ****************************** "
+
+#CONSTANT
+ROWS=3
+COLUMNS=3
+
+#DECLARE 2-DIMENSIONAL ARRAY
+declare -A board
+
+#FUNCTION TO RESET THE BOARD
+function resetBoard()
+{
+	for((rows=0; rows<$ROWS; rows++))
+	do
+		for((columns=0; columns<$COLUMNS; columns++))
+		do
+			board[$rows,$columns]=" "
+		done
+	done
+}
+
+resetBoard
